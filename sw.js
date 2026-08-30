6be1b5fd684f8f907e93cbe102419fc881027f97
diaz8938 <84084019+diaz8938@users.noreply.github.com>
2026-08-29 22:29:09 -0500

    Cache confidence-aware bulk references

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='extreme-coupon-v07-bulk';
-const CORE=['./','./index.html','./app.js','./planner.js','./bulk.js','./manifest.webmanifest','./icon.svg','./data/household.json','./data/comparisons.json','./data/coupons.json','./data/unit_catalog.json','./receipts/heb-2026-08-29.json','./receipts/dg-2026-08-29.json','./receipts/sams-2026-08-28.json'];
+const CACHE='extreme-coupon-v08-bulk';
+const CORE=['./','./index.html','./app.js','./planner.js','./bulk.js','./manifest.webmanifest','./icon.svg','./data/household.json','./data/comparisons.json','./data/coupons.json','./data/unit_catalog.json','./data/unit_competitors.json','./receipts/heb-2026-08-29.json','./receipts/dg-2026-08-29.json','./receipts/sams-2026-08-28.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
